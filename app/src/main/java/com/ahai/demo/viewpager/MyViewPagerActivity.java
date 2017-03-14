d2b9186bf75474df88e30f7959d7928fe4c8b959
fengzhenhai <zhenhai.fzh@alibaba-inc.com>
2017-03-15 01:36:51 +0800

    viewpager给item添加出场和入场动画

--- app/src/main/java/com/ahai/demo/viewpager/MyViewPagerActivity.java
+++ app/src/main/java/com/ahai/demo/viewpager/MyViewPagerActivity.java
@@ -8,8 +8,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -19,7 +23,7 @@ public class MyViewPagerActivity extends Activity implements View.OnClickListene
 
     private ViewPager viewPager;
     private List<View> views;
-    private Button btnDel;
+    private Button button;
     private MyViewPagerAdapter pagerAdapter;
 
     LinkedList<Integer> imgResIds = new LinkedList();
@@ -57,15 +61,75 @@ public class MyViewPagerActivity extends Activity implements View.OnClickListene
         pagerAdapter = new MyViewPagerAdapter(views);
         viewPager.setAdapter(pagerAdapter);
         viewPager.setCurrentItem(0);
-        btnDel = (Button) findViewById(R.id.btn_del_cur_pager);
-        btnDel.setOnClickListener(this);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == btnDel.getId()) {
-
+        if (v.getId() == button.getId()) {
+            startExitAnim();
         }
+    }
+
+    private void startExitAnim() {
+        View curChild = viewPager.getChildAt(viewPager.getCurrentItem());
+        int toYDelta = curChild.getHeight();
+        Log.d(TAG, "toYDelta:"+toYDelta);
+        Animation anim = makeTranslateAnimation(0, 0, 0, toYDelta, true);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startEnterAnim();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        curChild.startAnimation(anim);
+    }
+
+    private void startEnterAnim() {
+        int curIndex = viewPager.getCurrentItem();
+        Log.d(TAG, "curIndex:"+curIndex+", pageCount:"+viewPager.getChildCount());
+
+        View curChild = viewPager.getChildAt(curIndex);
+        View brother1 = viewPager.getChildAt(curIndex - 1);
+        View brother2 = viewPager.getChildAt(curIndex + 1);
+
+        int[] loc = new int[2];
+        curChild.getLocationOnScreen(loc);
+        int xCur = loc[0];
+
+        brother1.getLocationOnScreen(loc);
+        int xBrother1 = loc[0];
+
+        brother2.getLocationOnScreen(loc);
+        int xBrother2 = loc[0];
+
+        View brother = xBrother1 > xBrother2 ? brother1 : brother2;
+
+        int toXDelta = xCur - (xBrother1 > xBrother2 ? xBrother1 : xBrother2);
+
+        Log.d(TAG, "xCur:"+xCur+", xBrother1:"+xBrother1+", xBrother2:"+xBrother2+", toXDelta:"+toXDelta);
+        Animation anim = makeTranslateAnimation(0, toXDelta, 0, 0, true);
+        brother.startAnimation(anim);
+    }
+
+    private Animation makeTranslateAnimation(float fromX, float toX, float fromY, float toY, boolean fillAfter) {
+        Animation animation = new TranslateAnimation(fromX, toX, fromY, toY);
+        animation.setDuration(300);
+        animation.setFillEnabled(true);
+        animation.setFillAfter(fillAfter);
+        animation.setInterpolator(new LinearInterpolator());
+        return animation;
     }
 
     public class MyViewPagerAdapter extends PagerAdapter {
